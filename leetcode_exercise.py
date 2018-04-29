4b7e9e7b9318a986f8ba0693c2eb5ba916e05d4c
youmeizhang <youmei.zhang1@gmail.com>
2018-04-29 10:43:36 -0400

    Update leetcode_exercise.py

--- leetcode_exercise.py
+++ leetcode_exercise.py
@@ -227,3 +227,10 @@ def levelorder(root):
             myqueue.append(node.right)
         result.append(node.data)
     return result
+
+#Maximum Depth of Binary Tree
+def maxDepth(self, root):
+    if root == None:
+        return 0
+    else:
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1

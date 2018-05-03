889a41c130d639ff480f531fbbfb94f1b6a7a30c
youmeizhang <youmei.zhang1@gmail.com>
2018-05-03 17:22:37 -0400

    Update leetcode_exercise.py

--- leetcode_exercise.py
+++ leetcode_exercise.py
@@ -467,3 +467,17 @@ def findPeakElement(self, nums):
         else:
             right = mid - 1
     return left
+
+#Find Minimum in Rotated Sorted Array
+def findMin(self, nums):
+    if len(nums) == 1:
+        return nums[0]
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        mid = (left + right) / 2
+        if nums[mid] < nums[right]:
+            right = mid
+        else:
+            left = mid + 1
+    return nums[left]

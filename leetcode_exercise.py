3e77e20af251fce8b2a6cdb643bd8fd8e69d8cb3
youmeizhang <youmei.zhang1@gmail.com>
2018-05-03 17:22:15 -0400

    Update leetcode_exercise.py

--- leetcode_exercise.py
+++ leetcode_exercise.py
@@ -437,3 +437,33 @@ def search(self, nums, target):
             else:
                 right = mid - 1
     return -1
+
+#First Bad Version
+def firstBadVersion(self, n):
+    if n == 0:
+        return 0
+    left = 1
+    right = n
+    while left <= right:
+        mid = (left + right) / 2
+        if isBadVersion(mid):
+            if mid == 1 or not isBadVersion(mid-1):
+                return mid
+            right = mid - 1
+        else:
+            left = mid + 1
+#Find Peak Element
+def findPeakElement(self, nums):
+    if len(nums) == 1:
+        return 0
+    left = 0
+    right = len(nums) - 1
+    while left<right:
+        mid = (left + right) / 2
+        if nums[mid] > nums[mid-1] and nums[mid] > nums[mid + 1]:
+            return mid
+        elif nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left

d06f123d219df57b383d904b1b581d3f2bd6a848
youmeizhang <youmei.zhang1@gmail.com>
2018-06-25 20:58:39 -0400

    Update 10 Regular Expression Matching.py

--- 10 Regular Expression Matching.py
+++ 10 Regular Expression Matching.py
@@ -11,3 +11,35 @@ def isMatch(self, s, p):
             return self.isMatch(s[:-1], p[:-1])
         else:
             return False
+
+# DP Solution:
+'''
+if s[i-1] == p[j-1] or p[j-1] == ".":
+    T[i][j] = T[i-1][j-1]
+elif p[j-1] == "*":
+    T[i][j] = T[i][j-2]
+    if s[i-1] == p[j-1] or p[j-1] == ".":
+        T[i][j] = T[i][j] or T[i-1][j]
+        
+else:
+    T[i][j] = False
+'''
+
+class Solution(object):
+    def isMatch(self, s, p):
+        m = len(s)
+        n = len(p)
+
+        dp = [[False] * (n+1) for x in range(m+1)]
+        dp[0][0] = True
+
+        for j in range(2, n+1):
+            dp[0][j] = dp[0][j-2] and p[j-1] == "*"
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if p[j-1] == "*":
+                    dp[i][j] = dp[i][j-2] or dp[i][j-1] or (dp[i-1][j] and (s[i-1] == p[j-2] or p[j-2] == "."))
+                else:
+                    dp[i][j] = dp[i-1][j-1] and (s[i-1] == p[j - 1] or p[j-1] == ".")
+        return dp[-1][-1]

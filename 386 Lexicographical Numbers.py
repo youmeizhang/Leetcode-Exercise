b4e4cafe1cb4dc46e44f4a8550a70e703c5cc33b
youmeizhang <youmei.zhang1@gmail.com>
2018-06-27 20:11:04 -0400

    Create 386 Lexicographical Numbers.py

---
+++ 386 Lexicographical Numbers.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lexicalOrder(self, n):
+        res = []
+        stack = [1]
+        while stack:
+            x = stack.pop()
+            res.append(x)
+            if x < n and x % 10 < 9:
+                stack.append(x+1)
+            if x * 10 <= n:
+                stack.append(x*10)
+        return res

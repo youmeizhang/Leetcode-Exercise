25af34edbe577f37e38a2eef78a98678bafc54a4
youmeizhang <youmei.zhang1@gmail.com>
2018-07-19 20:49:26 -0400

    Create 437 Path Sum III.py

---
+++ 437 Path Sum III.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root, sum):
+        
+        def traverse(root, val):
+            if not root: return 0
+            res = (val == root.val)
+            res += traverse(root.left, val - root.val)
+            res += traverse(root.right, val - root.val)
+            return res
+        if not root: return 0
+        ans = traverse(root, sum)
+        ans += self.pathSum(root.right, sum)
+        ans += self.pathSum(root.left, sum)
+        return ans

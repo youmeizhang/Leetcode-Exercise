dbc2390c8801a5f8601e5625fb8180ae7cdf3380
Youmei Zhang <youmei.zhang1@gmail.com>
2018-09-04 22:55:35 -0400

    Update 542 01 Matrix.py

--- python/542 01 Matrix.py
+++ python/542 01 Matrix.py
@@ -1,5 +1,4 @@
-
-#error
+# unsolved error
 class Solution:
     def updateMatrix(self, matrix):
         h = len(matrix)
@@ -34,3 +33,31 @@ class Solution:
                 dfs(i, j)
 
         return dp
+    
+# Time limit
+class Solution:
+    def updateMatrix(self, matrix):
+        h, w = len(matrix), len(matrix[0])
+        ans = [[0] * w for x in range(h)]
+        queue = [(x, y) for x in range(h) for y in range(w) if matrix[x][y]]
+        step = 0
+        while queue:
+            step += 1
+            mqueue, nqueue = [], []
+            for x, y in queue:
+                zero = 0
+                for dx, dy in zip((1, 0, -1, 0), (0, 1, 0, -1)):
+                    nx = x + dx
+                    ny = y + dy
+                    if 0 <= nx < w and 0 <= ny < h and matrix[nx][ny] == 0:
+                        zero += 1
+                if zero:
+                    ans[x][y] = step
+                    mqueue.append((x, y))
+                else:
+                    nqueue.append((x, y))
+            for x, y in mqueue:
+                matrix[x][y] = 0
+            queue = nqueue
+        return ans
+

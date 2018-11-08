a260a510328ff0b44b743485c192e32b16766873
Youmei Zhang <youmei.zhang1@gmail.com>
2018-11-08 15:33:16 -0500

    Create 518 Coin Change 2.py

---
+++ python/518 Coin Change 2.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def change(self, amount, coins):
+        n = amount
+        c = coins
+        
+        if n < 0:
+            return 0
+        if n == 0:
+            return 1
+        if c == []:
+            return 0
+
+        dp = [[0] * (n+1) for i in range(len(c))]
+        for i in range(len(c)):
+            dp[i][0] = 1
+
+        for i in range(0, len(c)):
+            for j in range(1, n+1):
+                if j >= c[i]:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-c[i]]
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+        return dp[len(c)-1][n]

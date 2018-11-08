7a8c9553c6036f8da2aba9c8e99fba6e84c7a726
Youmei Zhang <youmei.zhang1@gmail.com>
2018-11-08 15:39:12 -0500

    Update 518 Coin Change 2.py

--- python/518 Coin Change 2.py
+++ python/518 Coin Change 2.py
@@ -22,3 +22,20 @@ class Solution(object):
                     dp[i][j] = dp[i-1][j]
 
         return dp[len(c)-1][n]
+
+class Solution(object):
+    def change(self, amount, coins):
+        n = amount
+        c = coins
+        if n == 0:
+            return 1
+        if c == []:
+            return 0
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for coin in c:
+            for i in range(1, n+1):
+                if coin <= i:
+                    dp[i] += dp[i - coin]
+        #print(dp)
+        return dp[n]

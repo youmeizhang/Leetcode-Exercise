22ac0679e0260c7aa271c44fdf544da36cd9969e
Youmei Zhang <youmei.zhang1@gmail.com>
2018-11-17 21:12:53 -0500

    Update Best Time to Buy and Sell Stock (k transaction).py

--- python/Best Time to Buy and Sell Stock (k transaction).py
+++ python/Best Time to Buy and Sell Stock (k transaction).py
@@ -10,3 +10,16 @@ class Solution(object):
                 maxdiff = max(maxdiff, dp[i-1][j] - prices[j])
                 dp[i][j] = max(dp[i][j-1], prices[j] + maxdiff)
         return dp[k][len(prices)-1]
+    
+#Time Limit
+class Solution(object):
+    def maxProfit(self, k, prices):
+        if len(prices) < 2 or k == 0:
+            return 0
+        dp = [[0] * len(prices) for i in range((k+1))]
+        for i in range(1, k+1):
+            for j in range(len(prices)):
+                for m in range(0, j):
+                    dp[i][j] = max(dp[i][j], dp[i][j-1], dp[i-1][m] + prices[j] - prices[m])
+
+        return dp[k][len(prices) - 1]

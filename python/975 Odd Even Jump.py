001bb35f4caf47238436b845f8dfa2307c528f21
Youmei Zhang <youmei.zhang1@gmail.com>
2019-01-13 22:38:56 -0500

    Update 975 Odd Even Jump.py

--- python/975 Odd Even Jump.py
+++ python/975 Odd Even Jump.py
@@ -53,3 +53,34 @@ class Solution(object):
                 res += 1
             i = original
         return res
+
+#credit to author: lee215
+class Solution(object):
+    def oddEvenJumps(self, A):
+        n = len(A)
+        next_higher = [0] * n
+        next_lower = [0] * n
+
+        stack = []
+        for a, i in sorted([a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_higher[stack.pop()] = i
+            stack.append(i)
+
+        stack = []
+        for a, i in sorted([-a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_lower[stack.pop()] = i
+            stack.append(i)
+
+        higher = [0] * n
+        lower = [0] * n
+
+        higher[-1] = 1
+        lower[-1] = 1
+
+        for i in range(len(A) - 1)[::-1]:
+            higher[i] = lower[next_higher[i]]
+            lower[i] = higher[next_lower[i]]
+
+        return sum(higher)

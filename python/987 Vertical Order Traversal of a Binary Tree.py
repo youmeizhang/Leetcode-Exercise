65307a66762e15017189f51093a7f8d39274bd66
Youmei Zhang <youmei.zhang1@gmail.com>
2019-02-04 22:15:22 -0500

    Update 987 Vertical Order Traversal of a Binary Tree.py

--- python/987 Vertical Order Traversal of a Binary Tree.py
+++ python/987 Vertical Order Traversal of a Binary Tree.py
@@ -1,23 +1,22 @@
 class Solution:
     def verticalTraversal(self, root: 'TreeNode') -> 'List[List[int]]':
 
-        self.dic = collections.defaultdict(int)
-        self.pos = [0, 0]
+        dic = collections.defaultdict(list)
         
         def tranverse(root, start, end):
-            if not root:
-                return
+            if root is not None:
+                
+                dic[start].append((end, root.val))
 
-            if self.dic[start] == 0:
-                self.dic[start] = [root.val]
-            else:
-                self.dic[start].append(root.val)
-
-            tranverse(root.left, start - 1, end - 1)
-            tranverse(root.right, start + 1, end - 1)
-        res = []
+                tranverse(root.left, start - 1, end + 1)
+                tranverse(root.right, start + 1, end + 1)
+            
+        
         tranverse(root, 0, 0)
-        for key, value in sorted(self.dic.items()):
-            res.append(value)
-
-        return res
+        for key in dic:
+            dic[key].sort()
+            
+        ks = dic.keys()
+        ks = sorted(ks)
+        
+        return [[s[1] for s in dic[key]] for key in ks]

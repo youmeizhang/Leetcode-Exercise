0f299aa120df87624765139dde39fbcc1533b5b8
Youmei Zhang <youmei.zhang1@gmail.com>
2019-02-07 21:45:10 -0500

    Create 672 Bulb Switcher II.py

---
+++ python/672 Bulb Switcher II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def flipLights(self, n: 'int', m: 'int') -> 'int':
+        if m == 0:
+            return 1
+        if n == 1:
+            return 2
+        if n == 2 and m == 1:
+            return 3
+        if n == 2:
+            return 4
+        if m == 1:
+            return 4
+        if m == 2:
+            return 7
+        if m >= 3:
+            return 8
+        return 8
+        

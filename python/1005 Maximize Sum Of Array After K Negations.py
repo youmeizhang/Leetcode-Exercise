dafb34f6a517dd0d001ae39e5e9b764dbe69b6d2
Youmei Zhang <youmei.zhang1@gmail.com>
2019-03-09 22:47:22 -0500

    Create 1005 Maximize Sum Of Array After K Negations.py

---
+++ python/1005 Maximize Sum Of Array After K Negations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestSumAfterKNegations(self, A, K):
+        n = len(A)
+        for i in range(1, K + 1): 
+            min = +2147483647
+            index = -1
+            for j in range(n): 
+
+                if (A[j] < min): 
+
+                    min = A[j] 
+                    index = j 
+
+            if (min == 0): 
+                break
+
+            A[index] = -A[index] 
+        sum = 0
+        for i in range(n): 
+            sum += A[i] 
+        return sum

60febc36a673553589042329ebd883d4debadd00
Youmei Zhang <youmei.zhang1@gmail.com>
2019-04-11 20:02:14 -0400

    Update 445 Add Two Numbers II.py

--- Microsoft/445 Add Two Numbers II.py
+++ Microsoft/445 Add Two Numbers II.py
@@ -64,3 +64,51 @@ class Solution(object):
         :rtype: ListNode
         """
         
+# credit to: https://blog.csdn.net/fuxuemingzhu/article/details/79380457
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        s1 = []
+        s2 = []
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+            
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+
+        carry = 0
+        head = ListNode(None)
+        dummy = head
+        value = 0
+        answer = None
+        
+        while s1 and s2:
+            p1 = s1.pop()
+            p2 = s2.pop()
+            value = p1 + p2 + carry
+            temp = answer
+            carry = value // 10
+            answer = ListNode(value % 10)
+            answer.next = temp
+            
+        l = s1 if s1 else s2
+        while l:
+            value = l.pop() + carry
+            temp = answer
+            carry = value // 10
+            answer = ListNode(value % 10)
+            answer.next = temp
+            
+        if carry :
+            temp = answer
+            answer = ListNode(carry)
+            answer.next = temp
+            
+        return answer

8dd5a534dd20156dcaa5f62bbc59ed7a9179f383
Youmei Zhang <youmei.zhang1@gmail.com>
2019-04-15 22:59:18 -0400

    Update 398 Random Pick Index.py

--- Yelp/398 Random Pick Index.py
+++ Yelp/398 Random Pick Index.py
@@ -24,3 +24,68 @@ class Solution(object):
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
 # param_1 = obj.pick(target)
+
+
+class Solution(object):
+
+    def __init__(self, nums):
+        self.nums = nums
+        self.dic = {}
+        
+        for i, num in enumerate(nums):
+            if num in self.dic:
+                self.dic[num].append(i)
+            else:
+                self.dic[num] = [i]
+                
+        """
+        :type nums: List[int]
+        """
+        
+
+    def pick(self, target):
+        
+        l = self.dic[target]
+        return l[random.randint(0, len(l) - 1)]
+                
+        """
+        :type target: int
+        :rtype: int
+        """
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
+
+class Solution(object):
+
+    def __init__(self, nums):
+        self.nums = nums   
+        """
+        :type nums: List[int]
+        """
+        
+
+    def pick(self, target):
+        res = None
+        count = 0
+        for i, num in enumerate(self.nums):
+            if num == target:
+                num = random.randint(0, count)
+                if num == 0:
+                    res = i
+                count += 1
+        return res
+                
+        """
+        :type target: int
+        :rtype: int
+        """
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)

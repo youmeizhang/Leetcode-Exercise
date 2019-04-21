3971e2f1825158ad669ab2ee98ffa23af69240bc
Youmei Zhang <youmei.zhang1@gmail.com>
2019-04-21 09:45:11 -0400

    Update 1029 Two City Scheduling.py

--- python/1029 Two City Scheduling.py
+++ python/1029 Two City Scheduling.py
@@ -1,1 +1,27 @@
-#updating
+class Solution(object):
+    def twoCitySchedCost(self, costs):
+        costs = sorted(costs, key = lambda x : -abs(x[0]-x[1]))
+        print(costs)
+        left = 0
+        right = 0
+        total = 0
+        n = len(costs)
+        mid = n // 2
+        for i in range(n):
+            if left < mid and right < mid:
+                if costs[i][1] >= costs[i][0]:
+                    total += costs[i][0]
+                    left += 1
+                else:
+                    total += costs[i][1]
+                    right += 1
+            else:
+                break
+        for j in range(i, n):
+            if left < mid:
+                total += costs[j][0]
+            elif right < mid:
+                total += costs[j][1]
+        return total
+
+

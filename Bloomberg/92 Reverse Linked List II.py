30f5e1f0feeea4e4631dd95592a3a9b83595af6f
Youmei Zhang <youmei.zhang1@gmail.com>
2019-04-25 22:39:40 -0400

    Create 92 Reverse Linked List II.py

---
+++ Bloomberg/92 Reverse Linked List II.py
@@ -0,0 +1,41 @@
+# credit to: https://blog.csdn.net/fuxuemingzhu/article/details/80794665
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        count = 1
+        root = ListNode(0)
+        root.next = head
+        pre = root
+        
+        while pre.next and count < m:
+            pre = pre.next
+            count += 1
+        if count < m:
+            return head
+        mNode = pre.next
+        curr = mNode.next
+        
+        while curr and count < n:
+            next = curr.next
+            curr.next = pre.next
+            pre.next = curr
+            mNode.next = next
+            curr = next
+            count += 1
+        return root.next
+        
+        
+        return dummy       
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        

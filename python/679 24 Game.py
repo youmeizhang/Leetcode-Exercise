9bf8053e91afafc6635dacaf31634673d7defcf9
Youmei Zhang <youmei.zhang1@gmail.com>
2019-06-01 11:25:06 -0400

    Update 679 24 Game.py

--- python/679 24 Game.py
+++ python/679 24 Game.py
@@ -1,3 +1,52 @@
+# credit to: https://www.youtube.com/watch?v=3zXaIOX1ezU
+
+class Solution(object):
+    def judgePoint24(self, nums):
+        new_nums = []
+        for x in nums:
+            new_nums.append(x * 1.0)
+            
+        return self.helper(new_nums)
+    
+    def helper(self, nums):
+        n = len(nums)
+        if n == 1:
+            return abs(nums[0] - 24) < 1e-6
+        
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+                else:
+                    a = nums[i]
+                    b = nums[j]
+                    tmp = a + b
+                    newNums = []
+                    for x in range(n):
+                        if x != i and x != j:
+                            newNums.append(nums[x])
+                            
+                    newNums.append(tmp)
+                    if self.helper(newNums):
+                        return True
+                    
+                    tmp = a - b
+                    newNums[-1] = tmp
+                    if self.helper(newNums):
+                        return True
+                    
+                    tmp = a * b
+                    newNums[-1] = tmp
+                    if self.helper(newNums):
+                        return True
+                    
+                    if b != 0:
+                        tmp = a / b
+                        newNums[-1] = tmp
+                        if self.helper(newNums):
+                            return True
+        return False
+    
 #credit to: http://bookshadow.com/weblog/2017/09/17/leetcode-24-game/
 
 class Solution(object):

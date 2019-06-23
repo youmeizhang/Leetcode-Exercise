782c03ff6286f3252ff259f57459e8cf70efaf3e
Youmei Zhang <youmei.zhang1@gmail.com>
2019-06-23 10:39:22 -0400

    Create 217 Contains Duplicate.cpp

---
+++ C++/217 Contains Duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int i = 0; i < nums.size(); i++) {
+            set.insert(nums[i]);
+        }
+        if (set.size() == nums.size()) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+};

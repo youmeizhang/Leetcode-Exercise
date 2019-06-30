e482c2b997871357042d61e8d1f719b7a9642b0f
Youmei Zhang <youmei.zhang1@gmail.com>
2019-06-30 15:03:19 -0400

    Update 1100 Find K-Length Substrings With No Repeated Characters.py

--- python/1100 Find K-Length Substrings With No Repeated Characters.py
+++ python/1100 Find K-Length Substrings With No Repeated Characters.py
@@ -1,1 +1,12 @@
-# upload after contest finishes
+class Solution:
+    def numKLenSubstrNoRepeats(self, S: str, K: int) -> int:
+        n = len(S)
+        if n < K:
+            return 0
+        count = 0
+        for i in range(n-K+1):
+            tmp = S[i:i+K]
+            if len(set(tmp)) == len(tmp):
+                count += 1
+        return count
+        

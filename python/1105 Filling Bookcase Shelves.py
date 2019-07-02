2bd70060ffb170b9c4babcdbec6a96f6db936974
Youmei Zhang <youmei.zhang1@gmail.com>
2019-07-01 21:00:41 -0400

    Create 1105 Filling Bookcase Shelves.py

---
+++ python/1105 Filling Bookcase Shelves.py
@@ -0,0 +1,19 @@
+# credit to: https://leetcode.com/problems/filling-bookcase-shelves/discuss/323415/simple-Python-DP-solution
+
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelf_width: int) -> int:
+        n = len(books)
+        dp = [float('inf') for _ in range(n+1)]
+        dp[0] = 0
+        
+        for i in range(1,n+1):
+            max_width = shelf_width
+            max_height = 0
+            j = i - 1
+            
+            while j >= 0 and max_width - books[j][0] >= 0:
+                max_width -= books[j][0]
+                max_height = max(max_height, books[j][1])
+                dp[i] = min(dp[i], max_height + dp[j])
+                j -= 1
+        return dp[n]

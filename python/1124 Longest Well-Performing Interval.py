bf9f1b13b1d939fcedf06ff378ec86ca9e891c43
Youmei Zhang <youmei.zhang1@gmail.com>
2019-07-14 10:12:02 -0400

    Update 1124 Longest Well-Performing Interval.py

--- python/1124 Longest Well-Performing Interval.py
+++ python/1124 Longest Well-Performing Interval.py
@@ -24,3 +24,25 @@ class Solution:
         return max(dp)
 
 
+# credit to: https://leetcode.com/problems/longest-well-performing-interval/discuss/334565/JavaC%2B%2BPython-O(N)-Solution-Life-needs-996-and-669
+
+class Solution:
+    def longestWPI(self, hours: List[int]) -> int:
+        seen = {}
+        score = 0
+        n = len(hours)
+        res = 0
+        for i in range(n):
+            if hours[i] > 8:
+                score += 1
+            else:
+                score -= 1
+            if score > 0:
+                res = i + 1
+            seen.setdefault(score, i)
+            
+            if score - 1 in seen:
+                res = max(res, i - seen[score - 1])
+        return res
+                
+        

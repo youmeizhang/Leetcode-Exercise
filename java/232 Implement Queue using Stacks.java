6033dc38cf78fb303690b50ccaf1d0f56868c173
Youmei Zhang <youmei.zhang1@gmail.com>
2019-10-12 22:15:06 -0700

    Update 232 Implement Queue using Stacks.java

--- java/232 Implement Queue using Stacks.java
+++ java/232 Implement Queue using Stacks.java
@@ -61,3 +61,49 @@ class MyQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+/** credit to: https://leetcode.com/problems/implement-queue-using-stacks/discuss/64206/Short-O(1)-amortized-C%2B%2B-Java-Ruby*/
+
+class MyQueue {
+    private Stack<Integer> input = new Stack<Integer>();
+    private Stack<Integer> output = new Stack<Integer>();
+        
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        peek();
+        return output.pop();
+        
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */

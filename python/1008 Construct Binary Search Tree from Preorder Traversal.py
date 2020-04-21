c31f47fe1c76eb88e027e51b609547a1545b6478
Youmei Zhang <youmei.zhang1@gmail.com>
2020-04-20 22:34:44 -0700

    Update 1008 Construct Binary Search Tree from Preorder Traversal.py

--- python/1008 Construct Binary Search Tree from Preorder Traversal.py
+++ python/1008 Construct Binary Search Tree from Preorder Traversal.py
@@ -1,5 +1,6 @@
 # credit to: https://blog.csdn.net/fuxuemingzhu/article/details/88379241
 
+# O(N^2)
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -25,5 +26,29 @@ class Solution(object):
         :type preorder: List[int]
         :rtype: TreeNode
         """
+        
+# credit to: https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/discuss/252232/JavaC%2B%2BPython-O(N)-Solution   
+
+# O(N) since construct is called exactly N times
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        self.i = 0
+        def construct(preorder, bound):
+            if self.i == len(preorder) or preorder[self.i] > bound:
+                return None
+            root = TreeNode(preorder[self.i])
+            self.i += 1
+            root.left = construct(preorder, root.val)
+            root.right = construct(preorder, bound)
+            return root
+        
+        return construct(preorder, float('inf'))
         
         

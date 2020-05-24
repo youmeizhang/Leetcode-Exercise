251c77e858fa9be96771a8c28f8e0d5a7d550f25
Youmei Zhang <youmei.zhang1@gmail.com>
2020-05-23 22:50:25 -0700

    Update 986 Interval List Intersections.py

--- python/986 Interval List Intersections.py
+++ python/986 Interval List Intersections.py
@@ -1,37 +1,27 @@
-class Solution:
-    def intervalIntersection(self, A: 'List[Interval]', B: 'List[Interval]') -> 'List[Interval]':
+# Important point: if lowest endpoint is in A then we know there is only interval in B can intersect with A. Because pair in B is disjoint. In this assumption, we can discard A after intersecting
+
+class Solution(object):
+    def intervalIntersection(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
         i = 0
         j = 0
-        
-        lena = len(A)
-        lenb = len(B)
+        m = len(A)
+        n = len(B)
         res = []
-        
-        while (i < lena - 1 and j < lenb - 1):
-            a = max(A[i].start, B[j].start)
-            b = min(A[i].end, B[j].end)
-            if a <= b:
-                res.append([a, b])
-            if B[j].end >= A[i+1].start:
+        while i < m and j < n:
+            min_value = max(A[i][0], B[j][0])
+            
+            if A[i][1] <= B[j][1]:
+                if min_value <= A[i][1]:
+                    res.append([min_value, A[i][1]])
                 i += 1
-            elif A[i].end >= B[j+1].start:
-                j += 1
             else:
-                i += 1
+                if min_value <= B[j][1]:
+                    res.append([min_value, B[j][1]])
                 j += 1
-        if i == lena - 1:        
-            while(j != lenb):
-                a = max(A[i].start, B[j].start)
-                b = min(A[i].end, B[j].end)
-                if a <= b:
-                    res.append([a, b])
-                j += 1
-        elif j == lenb - 1:
-            while(i != lena):
-                a = max(A[i].start, B[j].start)
-                b = min(A[i].end, B[j].end)
-                if a <= b:
-                    res.append([a, b])
-                i += 1
-     
         return res
+            

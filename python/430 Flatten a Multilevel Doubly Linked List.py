8476da2d2bd20cc0b8d4d5d514a616d7ca642bdb
Youmei Zhang <youmei.zhang1@gmail.com>
2020-07-10 22:50:54 -0700

    Create 430 Flatten a Multilevel Doubly Linked List.py

---
+++ python/430 Flatten a Multilevel Doubly Linked List.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        if not head:
+            return head
+        
+        stack = []
+        dummy = Node(0, None, head, None)
+        prev = dummy
+        stack.append(head)
+        
+        while stack:
+            root = stack.pop()
+            root.prev = prev
+            prev.next = root
+            
+            if root.next:
+                stack.append(root.next)
+                root.next = None
+            if root.child:
+                stack.append(root.child)
+                root.child = None
+            
+            prev = root
+        
+        dummy.next.prev = None
+        return dummy.next

38e5aa358f1489e5c5eebfe6b2f308e6c4283510
Youmei Zhang <youmei.zhang1@gmail.com>
2020-08-02 12:31:20 -0700

    Update 821 Shortest Distance to a Character.py

--- python/821 Shortest Distance to a Character.py
+++ python/821 Shortest Distance to a Character.py
@@ -1,3 +1,36 @@
+class Solution(object):
+    def shortestToChar(self, S, C):
+        """
+        :type S: str
+        :type C: str
+        :rtype: List[int]
+        """
+        n = len(S)
+        res = [float('inf')] * n
+        flag = False
+        dist = 0
+        for i in range(n):
+            if S[i] == C:
+                flag = True
+                dist  = 0
+            if flag:
+                res[i] = dist
+            dist += 1
+        print(res)
+        
+        dist = 0
+        flag = False
+        for i in range(n-1, -1, -1):
+            if S[i] == C:
+                flag = True
+                dist = 0
+                
+            if flag:
+                res[i] = min(dist, res[i])
+            dist += 1
+        
+        return res
+        
 class Solution:
     def shortestToChar(self, S: str, C: str) -> List[int]:
         n = len(S)

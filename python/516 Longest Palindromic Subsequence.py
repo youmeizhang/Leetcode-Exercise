4e98539631ddf3cd0ed46e63e09bdaa3db4e38a0
Youmei Zhang <youmei.zhang1@gmail.com>
2020-08-03 11:37:27 -0700

    Update 516 Longest Palindromic Subsequence.py

--- python/516 Longest Palindromic Subsequence.py
+++ python/516 Longest Palindromic Subsequence.py
@@ -1,3 +1,21 @@
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n-1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][-1]
+                    
+        
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)

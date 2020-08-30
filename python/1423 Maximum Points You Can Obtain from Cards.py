eb1434965c34a4ed49041663967ff2462ee89336
Youmei Zhang <youmei.zhang1@gmail.com>
2020-08-30 10:53:27 -0700

    Update 1423 Maximum Points You Can Obtain from Cards.py

--- python/1423 Maximum Points You Can Obtain from Cards.py
+++ python/1423 Maximum Points You Can Obtain from Cards.py
@@ -1,3 +1,26 @@
+# credit to: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/discuss/812818/Explained-DP-%2B-Window-Sliding-or-C%2B%2B
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        total= sum(cardPoints)
+        n = len(cardPoints)
+        i = 0
+        s = 0
+        while i < n-k:
+            s += cardPoints[i]
+            i += 1
+            
+        res = 0
+        res = max(res, total - s)
+        
+        while i < n:
+            s -= cardPoints[i-(n-k)]
+            s += cardPoints[i]
+            res = max(res, total - s)
+            i += 1
+        return res
+        
+
 class Solution(object):
     def maxScore(self, cardPoints, k):
         """

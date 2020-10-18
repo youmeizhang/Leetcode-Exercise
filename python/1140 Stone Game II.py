768d45eac056e4bb9e3ea07ee12853da86d95d27
Youmei Zhang <youmei.zhang1@gmail.com>
2020-10-18 12:46:21 -0700

    Update 1140 Stone Game II.py

--- python/1140 Stone Game II.py
+++ python/1140 Stone Game II.py
@@ -1,4 +1,5 @@
 # credit to: https://leetcode.com/problems/stone-game-ii/discuss/345354/Java-DP-with-memorization-easy-to-understand(with-explanation)
+# Min max problem
 
 class Solution(object):
     def stoneGameII(self, piles):
@@ -30,5 +31,30 @@ class Solution(object):
             return dp[i][M]
         
         return helper(0, 1)
+            
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        dp = [[0 for _ in range(n)] for _ in range(n+1)]
+        
+        def solve(s, M):
+            if s >= n:
+                return 0
+            M = min(n, M)
+            if dp[s][M]:
+                return dp[s][M]
+            if s + 2 * M >= n:
+                dp[s][M] = sum(piles[s:])
+                return dp[s][M]
+            
+            best = float('-inf')
+            curr = 0
+            for x in range(1, 2 * M + 1):
+                curr += piles[s+x-1]
+                best = max(best, curr - solve(s+x, max(x, M)))
+            dp[s][M] = best
+            return dp[s][M]
+        
+        return (sum(piles) + solve(0, 1)) // 2
             
         

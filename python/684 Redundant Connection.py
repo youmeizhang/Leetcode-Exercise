5c2fb3bde06e0623edbde0601e1d7d6686fd3179
Youmei Zhang <youmei.zhang1@gmail.com>
2021-02-15 12:01:51 -0800

    Update 684 Redundant Connection.py

--- python/684 Redundant Connection.py
+++ python/684 Redundant Connection.py
@@ -19,4 +19,43 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+
+# Original Union Find method
+class UnionFindSet:
+    def __init__(n):
+        self.parents = [0] * (n+1)
+        self.ranks = [0] * (n+1)
+        for i in range(n+1):
+            self.parents[i] = i        
+    
+    def union(x, y):
+        px = find(x)
+        py = find(y)
+        if px == py:
+            # no need to merge
+            return False
         
+        if self.ranks[px] > self.ranks[py]:
+            self.parents[py] = px
+        elif self.ranks[py] > self.ranks[px]:
+            self.parents[px] = py
+        else:
+            self.parents[py] = px
+            self.ranks[px] += 1
+        return True
+    
+    def find(x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents(x))
+        return self.parents[x]
+    
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        UnionFindSet(n)
+        
+        for edge in edges:
+            if not self.
+        
+    
+
